5b35ee02aa25aa939282e73e04f08015d36ebfd9
Jayant <188503548+jayant880@users.noreply.github.com>
2025-08-16 14:13:45 +0000

    Rework: Add lucid Icon to serach Bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,3 +1,4 @@
+import { Search } from "lucide-react";
 import React, { useState, type ChangeEvent } from "react"
 
 interface SearchBarProps {
@@ -23,7 +24,9 @@ const SearchBar: React.FC<SearchBarProps> = ({ getUsername }) => {
                 />
                 <button
                     onClick={handleSearch}
-                    className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 transtion">
+                    className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 transtion flex items-center"
+                >
+                    <Search className="w-5 h-5 mr-2" />
                     Search
                 </button>
             </div>
